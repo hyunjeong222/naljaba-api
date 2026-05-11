0c5ca0d871434157148954254e9314ea2f0b5e7e
jeongii <hyenj629@naver.com>
2026-05-11 18:47:12 +0900

    initial

---
+++ src/main/java/com/spring/naljaba_api/NaljabaApiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.naljaba_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaljabaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaljabaApiApplication.class, args);
+	}
+
+}
