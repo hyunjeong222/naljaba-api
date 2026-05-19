49cea7f5164275425eba92bb76c2df4b2539ad74
jeongii <hyenj629@naver.com>
2026-05-19 14:32:13 +0900

    feat: 멤버 기능 구현

---
+++ src/main/java/com/spring/naljaba_api/repository/MemberRepository.java
@@ -0,0 +1,18 @@
+package com.spring.naljaba_api.repository;
+
+import com.spring.naljaba_api.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface MemberRepository extends JpaRepository<Member, UUID> {
+    // 방장 존재 여부 확인
+    boolean existsByIsHostTrue();
+
+    // 중복 이름 확인
+    boolean existsByName(String name);
+
+    // 방장 조회
+    Optional<Member> findByIsHostTrue();
+}
