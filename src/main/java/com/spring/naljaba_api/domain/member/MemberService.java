5aa9c31365accac45318c681d0b1a40c72b49d99
jeongii <hyenj629@naver.com>
2026-05-23 00:47:46 +0900

    refactor: 이름 중복 검사 제거 및 프로필 색상 지정 방식 변경

--- src/main/java/com/spring/naljaba_api/domain/member/MemberService.java
+++ src/main/java/com/spring/naljaba_api/domain/member/MemberService.java
@@ -2,8 +2,6 @@ package com.spring.naljaba_api.domain.member;
 
 import com.spring.naljaba_api.dto.request.CreateMemberRequest;
 import com.spring.naljaba_api.dto.response.MemberResponse;
-import com.spring.naljaba_api.exception.CustomException;
-import com.spring.naljaba_api.exception.ErrorCode;
 import com.spring.naljaba_api.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,16 +18,11 @@ public class MemberService {
     // 회원 생성
     @Transactional
     public MemberResponse createMember(CreateMemberRequest request) {
-        // 중복 이름 확인
-        if (memberRepository.existsByName(request.name())) {
-            throw new CustomException(ErrorCode.DUPLICATE_NAME);
-        }
-
         // 방장 없으면 첫 번째 멤버 = 방장
         boolean isHost = !memberRepository.existsByIsHostTrue();
 
         Member member = memberRepository.save(
-                Member.create(request.name(), isHost)
+                Member.create(request.name(), request.profileColor(), isHost)
         );
 
         return MemberResponse.from(member);
--- src/main/java/com/spring/naljaba_api/dto/request/CreateMemberRequest.java
+++ src/main/java/com/spring/naljaba_api/dto/request/CreateMemberRequest.java
@@ -4,5 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 
 public record CreateMemberRequest (
     @NotBlank(message = "이름을 입력해주세요")
-    String name
+    String name,
+    String profileColor
 ) {}
